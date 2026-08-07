eb9c8f45041738bae388cf3ce10bd1a51f116f48
Afreen-707 <x423afreensayed@gmail.com>
2026-08-07 22:50:21 +0530

    UserSync added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs';
 //import TanStackProvider from "@/components/providers/TanStackProvider";
 import { Toaster } from "sonner";
+import UserSync from "@/components/UserSync";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -41,7 +42,7 @@ export default function RootLayout({
           <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
             {/* this is done in the home page component */}
             {/* <UserSync /> */}
-            <Toaster />
+            <UserSync />
             {children}
           </body>
         </html>
